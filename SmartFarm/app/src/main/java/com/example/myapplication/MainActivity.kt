864b87a3dc827c2dd7a90417b46bbdaace0d5b2b
codthestudent <hjun303@gnu.ac.kr>
2022-10-02 16:32:39 +0900

    FireBase 연동

--- SmartFarm/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ SmartFarm/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,15 +5,19 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 lateinit var navController: NavController
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
     }
 }
