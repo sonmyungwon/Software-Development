17cf017b5619073dbc8caed02c1d312f0dfabf92
codthestudent <hjun303@gnu.ac.kr>
2022-10-02 10:45:53 +0900

    앱 작동됨

--- SmartFarm/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ SmartFarm/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+
+lateinit var navController: NavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
 }
--- SmartFarm/app/src/main/java/com/example/myapplication/fragment/GreetingFragment.kt
+++ SmartFarm/app/src/main/java/com/example/myapplication/fragment/GreetingFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.myapplication.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -57,4 +59,5 @@ class GreetingFragment : Fragment() {
                 }
             }
     }
+
 }
