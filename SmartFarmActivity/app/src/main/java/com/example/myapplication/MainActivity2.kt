629ec0c31c4533cfb0d02363631737e0b1c0f2e4
codthestudent <hjun303@gnu.ac.kr>
2022-10-06 23:11:11 +0900

    fan, pump testing success

--- SmartFarmActivity/app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ SmartFarmActivity/app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -23,7 +25,7 @@ class MainActivity2 : AppCompatActivity() {
         val startBtn = findViewById<Button>(R.id.startBtn)
         startBtn.setOnClickListener{
             val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("/on_off/fan")
+            val myRef = database.getReference("/manual/sensor/pump")
 
             myRef.setValue(1)
 
@@ -31,9 +33,48 @@ class MainActivity2 : AppCompatActivity() {
         val startBtn2 = findViewById<Button>(R.id.startBtn2)
         startBtn2.setOnClickListener{
             val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("/on_off/fan")
+            val myRef = database.getReference("/manual/sensor/pump")
 
             myRef.setValue(0)
+
+        }
+        val startBtn3 = findViewById<Button>(R.id.startBtn3)
+        startBtn3.setOnClickListener{
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("/manual/sensor/fan")
+
+            myRef.setValue(1)
+
+        }
+        val startBtn4 = findViewById<Button>(R.id.startBtn4)
+        startBtn4.setOnClickListener{
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("/manual/sensor/fan")
+
+            myRef.setValue(0)
+
+        }
+        val autoControlBtn = findViewById<Button>(R.id.autoControlBtn)
+        autoControlBtn.setOnClickListener(){
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.auto_control_custom_dialog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("자동 제어 설정")
+
+            val mAlertDialog = mBuilder.show()
+            mAlertDialog.findViewById<Button>(R.id.button2)?.setOnClickListener(){
+                val database = FirebaseDatabase.getInstance()
+                val myRef = database.getReference("/manual/sensor/fan")
+
+                myRef.setValue(1)
+            }
+            mAlertDialog.findViewById<Button>(R.id.button3)?.setOnClickListener(){
+                val database = FirebaseDatabase.getInstance()
+                val myRef = database.getReference("/manual/sensor/fan")
+
+                myRef.setValue(0)
+            }
+
 
         }
 
