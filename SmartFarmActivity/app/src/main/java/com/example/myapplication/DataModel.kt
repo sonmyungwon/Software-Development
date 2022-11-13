38aaf695ee187289eeb159ffe5945df886f049df
codeTheStudent <96714243+codethestudent@users.noreply.github.com>
2022-11-13 21:46:44 +0900

    Delete DataModel.kt

--- SmartFarmActivity/app/src/main/java/com/example/myapplication/DataModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-import android.widget.EditText
-
-data class DataModel(
-        val temperature: EditText,
-        val soilHumidity: EditText,
-        val illuminance: EditText
-        )
